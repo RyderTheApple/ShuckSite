ddfb194e58280e4ba699b8a64a30c5e0c8457b49
RyderTheApple <105747027+RyderTheApple@users.noreply.github.com>
2025-11-17 18:17:50 -0600

    Update shuckmark.js

--- shuckmark.js
+++ shuckmark.js
@@ -5,6 +5,7 @@ shucks.setAttribute('src','https://i.redd.it/wo0o22rsr9hf1.gif');
 shucksimg.position ='fixed';
 shucksimg.width ='1000px';
 shucksimg.bottom ='1px';
+shucksimg.zIndex ='9999';
 audio.setAttribute('src','https://github.com/RyderTheApple/ShuckSite/raw/refs/heads/main/likebell2.mp3');
 audio.addEventListener('canplaythrough',()=>{
 document.body.appendChild(shucks);

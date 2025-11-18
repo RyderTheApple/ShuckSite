39aef94a94c5e207497eff6dd7a46e700431874e
RyderTheApple <105747027+RyderTheApple@users.noreply.github.com>
2025-11-17 18:03:35 -0600

    Create shuckmark.js

---
+++ shuckmark.js
@@ -0,0 +1,19 @@
+const shucks = document.createElement('img');
+const audio = document.createElement('audio');
+const shucksimg = shucks.style;
+shucks.setAttribute('src','https://i.redd.it/wo0o22rsr9hf1.gif');
+shucksimg.position ='fixed';
+shucksimg.width ='1000px';
+shucksimg.bottom ='1px';
+audio.setAttribute('src','https://github.com/RyderTheApple/ShuckSite/raw/refs/heads/main/likebell2.mp3');
+audio.addEventListener('canplaythrough',()=>{
+document.body.appendChild(shucks);
+document.body.appendChild(audio);
+audio.play();
+setTimeout(()=>{
+    shucks.remove();
+}, 3480);
+setTimeout(()=>{
+    audio.remove();
+},4000);
+});
